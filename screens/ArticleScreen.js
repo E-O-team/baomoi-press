d62621b39a862f1ae23b1c92981e00a9c8b2c679
NeoTheSecond <quangman1404@gmail.com>
2019-01-11 15:35:53 +0700

    REVERT

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -5,49 +5,18 @@ import CommentList from '../components/CommentList';
 import RecommendedList from '../components/RecommendedList';
 import {Consumer, Provider} from '../context/context.js'
 
-
-const FACEBOOK_ICON = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADwAAAA8CAMAAAANIilAAAAAYFBMVEUAAAAAQIAAWpwAX5kAX5gAX5gAX5gAXJwAXpgAWZ8AX5gAXaIAX5gAXpkAVaoAX5gAXJsAX5gAX5gAYJkAYJkAXpoAX5gAX5gAX5kAXpcAX5kAX5gAX5gAX5YAXpoAYJijtTrqAAAAIHRSTlMABFis4vv/JL0o4QvSegbnQPx8UHWwj4OUgo7Px061qCrcMv8AAAB0SURBVEjH7dK3DoAwDEVRqum9BwL//5dIscQEEjFiCPhubziTbVkc98dsx/V8UGnbIIQjXRvFQMZJCnScAR3nxQNcIqrqRqWHW8Qd6cY94oGER8STMVioZsQLLnEXw1mMr5OqFdGGS378wxgzZvwO5jiz2wFnjxABOufdfQAAAABJRU5ErkJggg==";
 export default class ArticleScreen extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            Article: {},
-            title: '',
+            Article: {}
         }
     }
     componentWillMount() {
         this.setState({
-            Article: this.props.navigation.getParam("Article", "ERR"),
-            title: "<h2>"+this.props.navigation.getParam("Article", "ERR").title.rendered+"</h2>",
+            Article: this.props.navigation.getParam("Article", "ERR")
         })
     }
-    onShare = () => {
-  Share.share({
-    ...Platform.select({
-      ios: {
-        message: 'Have a look on : ',
-        url: this.state.Article.link,
-      },
-      android: {
-        message: 'Have a look on : \n' + this.state.Article.link
-      }
-    }),
-    title: 'Wow, did you see that?'
-  }, {
-    ...Platform.select({
-      ios: {
-        // iOS only:
-        excludedActivityTypes: [
-          'com.apple.UIKit.activity.PostToTwitter'
-        ]
-      },
-      android: {
-        // Android only:
-        dialogTitle: 'Share : ' + this.state.Article.link
-      }
-    })
-  });
-}
     render(){
         return(
           <ScrollView style={{ flex: 1 , backgroundColor: 'white', padding: 10}}>
@@ -84,22 +53,3 @@ export default class ArticleScreen extends React.Component {
         );
     }
 };
-
-const styles = StyleSheet.create({
-  br:{
-    lineHeight: 5,
-  },
-  a: {
-    fontWeight: '300',
-    color: '#FF3366', // make links coloured pink
-  },
-  p: {
-    fontFamily: 'sans-serif-thin',
-    fontSize: 20,
-    lineHeight: 28,
-  },
-  instructions: {
-    marginTop: 20,
-    marginBottom: 20,
-  },
-});
