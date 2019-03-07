78e0ac99436828ee80361d9800a015e168a7fb7d
NeoTheSecond <quangman1404@gmail.com>
2019-03-07 19:44:04 +0700

    địt con mẹ eject expo đi

--- components/SideBar/MenuItemWithBadge.js
+++ components/SideBar/MenuItemWithBadge.js
@@ -7,7 +7,7 @@ import { Icon, Divider, Badge } from 'react-native-elements';
 
 export default class MenuItemWithBadge extends React.Component {
     render(){
-        const {name, type, color, content, textColor, exp, backGround} = this.props
+        const {name, type, color, content, textColor, exp, backgroundColor} = this.props
         return(
             <View style={{ height: 50, justifyContent: "space-between"}}>
                 <View></View>
@@ -20,7 +20,7 @@ export default class MenuItemWithBadge extends React.Component {
                         />
                         <Text style={{color: textColor, fontSize: 18, marginLeft: 10}} >{content}</Text>
                     </View>
-                        <Badge containerStyle={{ borderColor: "#f9b3b1", borderWidth: 1, backgroundColor: backGround}} wrapperStyle={{width: 80}}>
+                        <Badge containerStyle={{ borderColor: "#f9b3b1", borderWidth: 1, backgroundColor: backgroundColor}} wrapperStyle={{width: 80}}>
                             <Text style={{color: "#ff5756"}}>{exp}</Text>
                         </Badge>
                 </View>
